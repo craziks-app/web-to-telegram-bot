a68f83b27b8ff41bf67041ea38a522750e772091
craziks-app <68160029+craziks-app@users.noreply.github.com>
2023-05-25 14:29:23 +0530

    Update website-change-tg.py

--- website-change-tg.py
+++ website-change-tg.py
@@ -29,8 +29,9 @@ def report_change(url):
     html_response = (requests.get(url)).text
     soup = bs.BeautifulSoup(html_response,'lxml')
     txt = soup.select_one("p.English > a").get_text()
+    
     #select link of that text
-    link = soup.select_one("p.English > a")
+    #link = soup.select_one("p.English > a")
     notice = (re.sub(r'\n\s*\n', '\n', txt)).strip()
     
     file_name = ''.join(x for x in url if x.isalpha()) + ".txt"
@@ -45,14 +46,14 @@ def report_change(url):
             cache_file.write(notice)
             print("website change reported!")
             # Send the message (such as with a telegram bot provided below)
-            latest_notice = notice.split('\n', 1)[0] + link.get('href')
+            latest_notice = notice.split('\n', 1)[0] #+ link.get('href')
             print(latest_notice)
             telegram_bot_sendtext(latest_notice)
         else:
             print("no change")
     else:
         # Send the message (such as with a telegram bot provided below)
-        latest_notice = notice.split('\n', 1)[0] + link.get('href')
+        latest_notice = notice.split('\n', 1)[0] #+ link.get('href')
         print(latest_notice)
         res = telegram_bot_sendtext(latest_notice)
         print(res)
