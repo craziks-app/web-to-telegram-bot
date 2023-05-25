70288b23a544de0610f3ba61610e0748bc208a4a
craziks-app <68160029+craziks-app@users.noreply.github.com>
2023-05-25 15:27:41 +0530

    Update website-change-tg.py

--- website-change-tg.py
+++ website-change-tg.py
@@ -32,9 +32,9 @@ def report_change(url):
     
     ####################################################select link of that text#####################################################
     #link = soup.select_one("p.English > a")
-    test = soup.select_one("a").get('href')
+    tester = soup.select_one("p.English > a").get('href')
     notice = (re.sub(r'\n\s*\n', '\n', txt)).strip()
-    
+    test = (re.sub(r'\n\s*\', '\n', tester)).strip()
     file_name = ''.join(x for x in url if x.isalpha()) + ".txt"
 
     # Check if file exists that matches the page's content
